743a8c7d06c15ccc1d3fd771592e17a810b4402b
Jeremy Boetticher <me4jeremyboett@gmail.com>
2021-12-06 20:39:26 -0600

    btree index

--- src/btree.cpp
+++ src/btree.cpp
@@ -26,6 +26,8 @@ namespace badgerdb
 // BTreeIndex::BTreeIndex -- Constructor
 // -----------------------------------------------------------------------------
 
+
+
 BTreeIndex::BTreeIndex(const std::string & relationName,
 		std::string & outIndexName,
 		BufMgr *bufMgrIn,
@@ -41,19 +43,37 @@ BTreeIndex::BTreeIndex(const std::string & relationName,
 	std::string indexName = idxStr.str(); // indexName is the name of the index file
 	outIndexName = indexName;
 
-
-
-	// @TODO: initialize the BTree data structure
-
-
-
 	// if indexName exists, then the file is opened. Else, a new index file is created.
 	try {
-		BlobFile bf = BlobFile.create(indexName); 
+		file = &(BlobFile.create(indexName));
+
+		// index file doesn't already exist:
+		IndexMetaInfo newInfo;
+		newInfo.relationName = relationName;
+		newInfo.attrByteOffset = attrByteOffset;
+		newInfo.attrType = attrType;
+		newInfo.rootPageNo = 1;
+		
+		Page metaPage = (Page) newInfo;
+		file->writePage(0, metaPage);
+
+		NonLeafNodeInt root;
+		root.level = 0;
+		Page rootPage = (Page) root;
+		file->writePage(1, rootPage);
+
+		rootPageNum = 1;
 	}
 	catch(FileExistsException exists) {
-		std::cout << "Index file " << indexName << " already exists!";
+		// index file already exists:
+		file = &(BlobFile.open(indexName));
+
+		// read meta info (btree.h:108)
+		IndexMetaInfo metaInfo = (IndexMetaInfo)(file->readPage(0));
+		rootPageNum = metaInfo.rootPageNo;
 	}
+
+
 
 	// the constructor should scan relationName and insert entries
 	// for all of the tuples in the relation into the index
@@ -109,19 +129,48 @@ BTreeIndex::~BTreeIndex()
 // BTreeIndex::insertEntry
 // -----------------------------------------------------------------------------
 
+/**
+ * @brief Gets a leaf node, assumes that it's a non leaf node.
+ * 
+ * @param pageId the pageId of the non leaf node
+ * @return NonLeafNodeInt the struct representing the non leaf node
+ */
+NonLeafNodeInt BTreeIndex::getNonLeafNodeFromPage(PageId pageId) {
+	Page* p = &(blobFile.readPage(pageId));
+	NonLeafNodeInt node = (NonLeafNodeInt)(p);
+	return node;
+}
+
+/**
+ * @brief Gets the root node
+ * 
+ * @return NonLeafNodeInt 
+ */
+NonLeafNodeInt BTreeIndex::getRootNode() {
+	return getNonLeafNodeFromPage(rootPageNum);
+}
 
 void BTreeIndex::insertEntry(const void *key, const RecordId rid) 
 {
 	/*
 	Start from root and recursively search for which leaf, key belongs to
 	If leaf is full then split leaf, update parent non-leaf, and if root needs splitting then update metadata
+	Might have to keep track of height here
 	*/
-	NonLeafNodeInt nonleaf;
-	nonleaf.level = 0
-	nonleaf.keyArray 
-	if (key > IndexMetaInfo.rootPageNo) {
+	NonLeafNodeInt root = getRootNode();
+	
+	if (BTreeIndex rootPageNum == NULL) {
+	NonLeafNodeInt root = getRootNode();
+	}
+	
+	if (key < rootPageNum) {
 		insertEntry()
 	}
+	else if (key > rootPageNum)
+	{
+		
+	}
+	
 }
 
 // -----------------------------------------------------------------------------
@@ -134,8 +183,22 @@ void BTreeIndex::startScan(const void* lowValParm,
 				   const Operator highOpParm)
 {
 	if(lowValParm > highValParm) throw BadScanrangeException();
+	if(lowOpParm != Operator.LT && lowOpParm != Operator.LTE) throw BadOpcodesException();
+	if(highOpParm != Operator.GT && highOpParm != Operator.GTE) throw BadOpcodesException();
 
+	// TODO: check if scan has already started?
+
+	scanExecuting = true;
 	
+
+	// Sets data in the provided parameters for scanNext()
+	lowValInt	= *(int*)lowValParm;
+	highValInt	= *(int*)highValParm;
+	lowOp 		= lowOpParm;
+	highOp		= highOpParm;
+
+	// Get the root to start the scan
+	NonLeafNodeInt root = getRootNode();
 
 	/*
 	This method is used to begin a “filtered scan” of the index. For example, if the
@@ -152,11 +215,17 @@ void BTreeIndex::startScan(const void* lowValParm,
 void BTreeIndex::scanNext(RecordId& outRid) 
 {
 	/*
-	This method is used to begin a “filtered scan” of the index. For example, if the
-	method is called using arguments (1,GT,100,LTE), then the scan should seek all
-	entries greater than 1 and less than or equal to 100.
+	This method fetches the record id of the next tuple that matches the scan crite-
+	ria. If the scan has reached the end, then it should throw the following excep-
+	tion: IndexScanCompletedException. For instance, if there are two data en-
+	tries that need to be returned in a scan, then the third call to scanNext must throw
+	IndexScanCompletedException. A leaf page that has been read into the buffer
+	pool for the purpose of scanning, should not be unpinned from buffer pool unless
+	all records from it are read or the scan has reached its end. Use the right sibling
+	page number value from the current leaf to move on to the next leaf which holds
+	successive key values for the scan.
 	*/
-
+	
 	
 }
 
