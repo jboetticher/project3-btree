49f4a64551a4403fb40b9c4105b5e0cfc8b17a6f
Jeremy Boetticher <me4jeremyboett@gmail.com>
2021-12-07 11:43:12 -0600

    wrote code for scan

--- src/btree.cpp
+++ src/btree.cpp
@@ -136,7 +136,7 @@ BTreeIndex::~BTreeIndex()
  * @return NonLeafNodeInt the struct representing the non leaf node
  */
 NonLeafNodeInt BTreeIndex::getNonLeafNodeFromPage(PageId pageId) {
-	Page* p = &(blobFile.readPage(pageId));
+	Page* p = &(file->readPage(pageId));
 	NonLeafNodeInt node = (NonLeafNodeInt)(p);
 	return node;
 }
@@ -173,6 +173,46 @@ void BTreeIndex::insertEntry(const void *key, const RecordId rid)
 	
 }
 
+/**
+ * @brief Finds the closest pageId to the query.
+ * 
+ * @param node the node whose children to search in
+ * @param lowValParam the low value to compare to
+ * @param greaterThan the greater value to compare to
+ * @return PageId the pageId of the page that might have the values desired
+ */
+PageId BTreeIndex::findLeastPageId(NonLeafNodeInt node, int lowValParam, Operator greaterThan) {
+	for (int i = 0; i < node.keyArray.length; i++)
+	{
+		int key = node.keyArray[i];
+		bool comparison;
+		
+		// Determine correct operator comparison
+		if(greaterThan == Operator.GT) 
+			comparison = key > lowValParam;
+		else if(greaterThan == Operator.GTE)
+			comparison = key >= lowValParam;
+		else {
+			throw BadOpcodesException();
+		}
+
+		// if i = 0, then it can only return key[0].
+		// if i > 0, then it can return key[i-1], because there may be elements before.
+		// Example:	10 >=					12
+		//				8, 23		  	  29, 35	       42, 50
+		//		8, 9, 10	23, 25	 29, 31    35, 40   42, 45   50, 51	
+		// If we chose 23 on level 1, then we would have skipped 10 on level 2.
+		if(comparison) {
+			if(i > 0) return node.pageNoArray[i - 1];
+			else return node.pageNoArray[i];
+		}
+		else if(i == node.keyArray.length - 1) {
+			// if it's the very last in the b+ tree, then we can only go to the very right.
+			return node.pageNoArray[i];
+		}
+	}
+}
+
 // -----------------------------------------------------------------------------
 // BTreeIndex::startScan
 // -----------------------------------------------------------------------------
@@ -182,14 +222,19 @@ void BTreeIndex::startScan(const void* lowValParm,
 				   const void* highValParm,
 				   const Operator highOpParm)
 {
+	/*
+	This method is used to begin a “filtered scan” of the index. For example, if the
+	method is called using arguments (1,GT,100,LTE), then the scan should seek all
+	entries greater than 1 and less than or equal to 100.
+	*/
+
 	if(lowValParm > highValParm) throw BadScanrangeException();
-	if(lowOpParm != Operator.LT && lowOpParm != Operator.LTE) throw BadOpcodesException();
-	if(highOpParm != Operator.GT && highOpParm != Operator.GTE) throw BadOpcodesException();
+	if(lowOpParm != Operator.GT && lowOpParm != Operator.GTE) throw BadOpcodesException();
+	if(highOpParm != Operator.LT && highOpParm != Operator.LTE) throw BadOpcodesException();
 
 	// TODO: check if scan has already started?
 
 	scanExecuting = true;
-	
 
 	// Sets data in the provided parameters for scanNext()
 	lowValInt	= *(int*)lowValParm;
@@ -198,14 +243,51 @@ void BTreeIndex::startScan(const void* lowValParm,
 	highOp		= highOpParm;
 
 	// Get the root to start the scan
-	NonLeafNodeInt root = getRootNode();
+	NonLeafNodeInt nextNode = getRootNode();
+	int highestLevel = 3; // TODO: find the highest level so that it can cast to leaf node
+	LeafNodeInt leaf;
 
-	/*
-	This method is used to begin a “filtered scan” of the index. For example, if the
-	method is called using arguments (1,GT,100,LTE), then the scan should seek all
-	entries greater than 1 and less than or equal to 100.
-	*/
-	
+	// scan continues until a proper leaf node is found
+	while (1)
+	{
+		if(nextNode.level + 1 == highestLevel) {
+			// if the next node is the last level, then the next level has leaf nodes.
+			PageId pageId = findLeastPageId(nextNode, lowValInt, lowOpParm);
+			Page* p = &(file->readPage(pageId));
+			leaf = (LeafNodeInt)(p);
+
+			// it also happens to have the pageNum we're looking for
+			currentPageNum = pageId;
+			currentPageData = p;
+			break;
+		}
+		else {
+			PageId pageId = findLeastPageId(nextNode, lowValInt, lowOpParm);
+			nextNode = getNonLeafNodeFromPage(pageId);
+		}
+	}
+
+	// scan continues until a proper leaf node is found for scanNext
+	int leafRidLength = sizeof(leaf.ridArray) / sizeof(leaf.ridArray[0]);
+	for (int i = 0; i < leafRidLength; i++)
+	{
+		// Determine correct operator comparison
+		int key = leaf.keyArray[i];
+		bool comparison;
+		if(lowOp == Operator.GT) 
+			comparison = key > lowValParam;
+		else if(lowOp == Operator.GTE)
+			comparison = key >= lowValParam;
+
+		// sets the next entry + sets page data
+		if(comparison) {
+			nextEntry = i;
+			break;
+		}
+	}
+
+	// nothing satisifies the scan
+	nextEntry = -1;
 }
 
 // -----------------------------------------------------------------------------
@@ -225,8 +307,50 @@ void BTreeIndex::scanNext(RecordId& outRid)
 	page number value from the current leaf to move on to the next leaf which holds
 	successive key values for the scan.
 	*/
+
+	// NOTE: I'm not sure how to "unpin" a page. I assume if there were a funciton that
+	// did that, then you would do it in the second if statement (nextEntry >= leafRidLength).
 	
-	
+	if(nextEntry < 0) throw IndexScanCompletedException();
+
+	LeafNodeInt leaf = (LeafNodeInt)(p);
+	int leafRidLength = sizeof(leaf.ridArray) / sizeof(leaf.ridArray[0]);
+
+	// move on to the next page, or throw an error
+	if(nextEntry >= leafRidLength) {
+		// I assume that if rightSibPageNo = 0, then it's "null", indicating that there
+		// is no next page. If that is the case, the scan must be done.
+		if(leaf.rightSibPageNo == 0) throw IndexScanCompletedException();
+
+		PageId nextPage = leaf.rightSibPageNo;
+		currentPageNum = nextPage;
+		currentPageData = &(file.readPage(nextPage));
+		nextEntry = 0;
+
+		// I'm getting recursive here. Not sure if I did the & right
+		// The idea is we redo the process now that the next page is set.
+		scanNext(outRid); 
+		return;
+	}
+
+	// No need to check for greater than, because that has already happened in the
+	// startScan function. We only need to check lesser than.
+	int key = leaf.keyArray[nextEntry];
+	bool comparison;
+	if(highOp == Operator.LT) 
+		comparison = key < highValInt;
+	else if(highOp == Operator.LTE)
+		comparison = key <= highValInt;
+
+	// if the comparison holds true, return it and go to the next entry.
+	// otherwise, our scan is completed.
+	if(comparison) {
+		outRid = leaf.ridArray[nextEntry];
+		nextEntry++;
+	}
+	else {
+		throw IndexScanCompletedException();
+	}
 }
 
 // -----------------------------------------------------------------------------
@@ -235,7 +359,19 @@ void BTreeIndex::scanNext(RecordId& outRid)
 //
 void BTreeIndex::endScan() 
 {
+	/*
+	This method terminates the current scan and unpins all the pages that have been
+	pinned for the purpose of the scan. It throws ScanNotInitializedException
+	when called before a successful startScan call.  
+	*/
 
+	if(!scanExecuting) {
+		throw ScanNotInitializedException();
+	}
+
+	// TODO: unpin all the pages
+
+	scanExecuting = false;
 }
 
 }
